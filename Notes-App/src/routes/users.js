1f1043bf7a07991cc6d2573f6c5197ee5d7fceff
shir111 <ariasv.shirley@gmail.com>
2020-05-25 16:28:56 +0200

    Notes App 1st commit

---
+++ Notes-App/src/routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/users/signin', (req, res) => {
+    res.render('users/signin');
+});
+
+router.get('/users/signup', (req, res) => {
+    res.render('users/signup');
+});
+
+router.post('/users/signup', (req, res) => {
+    console.log(req.body);
+    res.send('ok');
+});
+
+
+module.exports = router;
